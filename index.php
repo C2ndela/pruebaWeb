5334db1ad82514c15779aa248b567fbb4636b6d0
C2ndela <strawberry.bbbgum@gmail.com>
2022-07-03 01:11:59 -0300

    agregado de color

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 
-echo ("<hi style 'color:red;'> Hola </hi>");
+echo ("<hi style"" 'color:red;'> Hola </hi>");
 
-echo ("<h2 style'color:orange';> UTN-FRA</h2>");
+echo ("<h2 style""'color:orange';> UTN-FRA</h2>");
 
 ?>
